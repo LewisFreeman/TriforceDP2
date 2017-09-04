029bfa8335cd6d739b2e4c1f6a9b37abe65d2828
Stephen Fleming <100963909@student.swin.edu.au>
2017-09-04 20:20:55 +1000

    request php file

--- js/SalesController.js
+++ js/SalesController.js
@@ -1,11 +1,26 @@
-var myApp = angular.module('myApp',[]);
+var myApp = angular.module('salesRecordsController', []);
 
-myApp.controller('SalesController', ['$scope', function($scope) {
-    $scope.salesRecords = {};
+myApp.controller("salesRecordsController",
+    function ($scope, $http) {
+        $scope.salesRecords = {};
+
+        $scope.loadRecordsFromDB = function () {
+            $http({
+                method: 'GET',
+                url: '../php/getSalesRecords.php'
+            }).then(function successCallback(response) {
+                // this callback will be called asynchronously
+                // when the response is available
+                $scope.salesRecords = response.data;
+                console.log("response recieved from getSalesRecords");
+                
+            }, function errorCallback(response) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+                console.log("no response recieved");
+                
+            });
+        };
     
-    $scope.getSalesRecords = function() {
-        console.log("TODO: Get records from database and assign to $scope.salesRecords");
-        
-        //
-    }; 
-}]);
+        $scope.loadRecordsFromDB();
+});
