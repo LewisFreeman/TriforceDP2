46fbeff37e130de09292b44a173101239a0eeb20
Stephen Fleming <100963909@student.swin.edu.au>
2017-09-04 20:54:39 +1000

    attempting to retrieve data from php

--- js/SalesController.js
+++ js/SalesController.js
@@ -1,26 +1,38 @@
+console.log("sales controller loaded");
+
 var myApp = angular.module('salesRecordsController', []);
 
-myApp.controller("salesRecordsController",
-    function ($scope, $http) {
-        $scope.salesRecords = {};
+myApp.controller('salesRecordsController', ['$scope', function ($scope) {
+        console.log("controller scope start");
+        $scope.salesRecords = null;
 
         $scope.loadRecordsFromDB = function () {
+            console.log("begin requesting data");
+
             $http({
                 method: 'GET',
                 url: '../php/getSalesRecords.php'
             }).then(function successCallback(response) {
+                console.log("response recieved from getSalesRecords");
+                console.log(response);
                 // this callback will be called asynchronously
                 // when the response is available
                 $scope.salesRecords = response.data;
-                console.log("response recieved from getSalesRecords");
-                
+
             }, function errorCallback(response) {
                 // called asynchronously if an error occurs
                 // or server returns response with an error status.
                 console.log("no response recieved");
-                
+
             });
+
+            console.log("end requesting data");
+            console.log($scope.salesRecords);
         };
-    
+
         $scope.loadRecordsFromDB();
-});
+        console.log("controller scope end");
+    }
+]);
+
+console.log("sales controller end");
