b0408700ac9e852c7c3d0e061187939d9346b8fd
Stephen Fleming <100963909@student.swin.edu.au>
2017-09-05 13:56:09 +1000

    sales records now display

--- js/SalesController.js
+++ js/SalesController.js
@@ -1,38 +1,33 @@
 console.log("sales controller loaded");
 
-var myApp = angular.module('salesRecordsController', []);
+var myApp = angular.module('PHPSRePS', []);
 
-myApp.controller('salesRecordsController', ['$scope', function ($scope) {
-        console.log("controller scope start");
-        $scope.salesRecords = null;
+myApp.controller('salesRecordsController', function ($scope, $http) {
+    console.log("controller scope start");
+    $scope.salesRecords = [];
 
-        $scope.loadRecordsFromDB = function () {
-            console.log("begin requesting data");
+    console.log("begin requesting data");
 
-            $http({
-                method: 'GET',
-                url: '../php/getSalesRecords.php'
-            }).then(function successCallback(response) {
-                console.log("response recieved from getSalesRecords");
-                console.log(response);
-                // this callback will be called asynchronously
-                // when the response is available
-                $scope.salesRecords = response.data;
+    $http({
+        method: 'GET',
+        url: 'http://localhost:80/TriforceDP2/php/getSalesRecords.php'
+    }).then(function successCallback(response) {
+        console.log("response recieved from getSalesRecords");
+        console.log(response);
+        // this callback will be called asynchronously
+        // when the response is available
+        $scope.salesRecords = [response.data];
 
-            }, function errorCallback(response) {
-                // called asynchronously if an error occurs
-                // or server returns response with an error status.
-                console.log("no response recieved");
+    }, function errorCallback(response) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+        console.log("no response recieved");
 
-            });
+    });
 
-            console.log("end requesting data");
-            console.log($scope.salesRecords);
-        };
-
-        $scope.loadRecordsFromDB();
-        console.log("controller scope end");
-    }
-]);
+    console.log("end requesting data");
+    console.log($scope.salesRecords);
+    console.log("controller scope end");
+});
 
 console.log("sales controller end");
