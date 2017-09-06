20f2fddf1dffa5612f65d30df42cb5b394a9c51a
Stephen Fleming <100963909@student.swin.edu.au>
2017-09-06 20:56:30 +1000

    all sales records now display

--- php/getSalesRecords.php
+++ php/getSalesRecords.php
@@ -1,11 +1,15 @@
 <?php 
-    $db_con = new mysqli("127.0.0.1", "root", "", "PHPSRePS");
-    $table = "SalesRecords";
+	$db_con = new mysqli("127.0.0.1", "root", "", "PHPSRePS");
+	$table = "SalesRecords";
 
-    $result = $db_con->query("SELECT * FROM $table;");
-    $rows = $result->fetch_assoc();
+	$result = $db_con->query("SELECT * FROM $table;");
+	//$rows = $result->fetch_assoc();
+	$rows = array();
+	while ($r = $result->fetch_assoc()) {
+		$rows[] = $r;
+	}
 
-    //stackoverflow.com/questions/25064570/get-json-data-from-a-php-file-for-an-angular-scope
-    header('Content-Type: application://json');
-    echo json_encode($rows);
+	//stackoverflow.com/questions/25064570/get-json-data-from-a-php-file-for-an-angular-scope
+	header('Content-Type: application://json');
+	echo json_encode($rows);
 ?>
