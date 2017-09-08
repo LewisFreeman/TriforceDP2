a25577d0532dde99058d5dc95aba24de5baf0357
Lewis Freeman <LewisWMFreeman@gmail.com>
2017-09-08 16:39:36 +1000

    Comments added my pages

--- POS.php
+++ POS.php
@@ -5,10 +5,11 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="css/bootstrap.min.css" rel="stylesheet" />
+    <!-- Custom stylesheet -->
     <link href="css/styles.css" rel="stylesheet" />
     <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
+    <!-- Custom controller -->
     <script src="js/POSController.js"></script>
-      
   </head>
   <body>
     <div class="container" data-ng-controller="myCtrl">
@@ -21,10 +22,12 @@
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
             </button>
+            <!-- Banner item (may replace with logo at later point) -->
             <a class="navbar-brand" href="#">PHP-SRePS</a>
           </div>
         </div>
         <div class="collapse navbar-collapse" id="navbar-collapse-1">
+          <!-- Nav bar items list -->
           <ul class="nav navbar-nav">
             <li><a href="Home.html">Home </a></li>
             <li><a href="Reports.html">Reports</a></li>
@@ -38,17 +41,20 @@
       </nav>
       <div class="row">
         <h1>Checkout</h1>
-        <p>{{M}}</p>
       </div>
       <div class="row well">
         <div class="col-lg-6">
           <div class="row">
             <div class="col-lg-12">
+              <!-- Border-bottom is a custom style class -->
               <h3 class="border-bottom">Cart</h3>
               <br>
+              <!-- "Nothing here yet" will only display if the cart is empty -->
               <div class="text-center" ng-show="Cart.length == 0">
                 <p>Nothing here yet</p>
               </div>
+              <!-- Add a row foreach object in the cart array -->
+              <!-- Display the name, amount and price of each -->
               <div class="row" ng-repeat="c in Cart">
                 <div class="col-xs-5 col-sm-3 col-lg-3 border-right text-center">
                   <p>{{c.name}}</p>
@@ -61,6 +67,7 @@
                 </div>
                 <div class="col-xs-offset-6 col-sm-offset-8 col-lg-offset-8">
                   <p>
+                    <!-- Delete button to remove items from the cart -->
                     <button type="button" class="btn btn-danger" ng-click="Delete(c)">Delete</button>
                   </p>
                 </div>
@@ -79,6 +86,8 @@
               </div>
               <div class="row">
                 <div class="col-lg-12">
+                  <!-- Drop down list of items -->
+                  <!-- Loaded from the Items array in the controller -->
                   <label for="Item">Select Item:</label>
                   <select class="form-control" id="Item" data-ng-model="Item">
                     <option ng-repeat="i in Items" value="{{i.name}}">{{i.name}}</option>
@@ -87,6 +96,7 @@
               </div>
               <div class="row">
                 <div class="col-lg-12">
+                  <!-- Number input for the amount of items required -->
                   <label for="Number" class="col-2 col-form-label">Number</label>
                   <input class="form-control" type="number" value="1" id="Number" data-ng-model="Number">
                 </div>
@@ -94,7 +104,10 @@
               <div class="row">
                 <div class="col-lg-12">
                   <br>
+                  <!-- Error message output (will only show if error has a value, default is null) -->
                   <p class="text-danger">{{error}}</p>
+                  <!-- Add button will put the selected values into the cart array to be displayed and checkedout -->
+                  <!-- Discard will clear the input fields -->
                   <button type="button" class="btn btn-danger btn-md" ng-click="Item = null; Number = null">Discard</button>
                   <button type="button" class="btn btn-success btn-md" ng-click="Add(Item, Number)">Add</button>
                 </div>
@@ -105,16 +118,20 @@
       </div>
       <div class="row row well">
         <div class="col-sm-4 col-lg-4 text-center">
+          <!-- Outputs the total amount by calling the GetTotal function in the controller -->
           <p class="total">Total Amount = ${{GetTotal()}}</p>
         </div>
         <div class="col-sm-4 col-lg-4 text-center border-right border-left">
+          <!-- Clear button will clear all fields and use the ClearAll function in the controller to empty the cart-->
           <button type="button" class="btn btn-danger btn-lg" ng-click="Item = null; Number = null; ClearAll()">Clear All</button>
         </div>
         <div class="col-sm-4 col-lg-4 text-center">
+          <!-- Checkout button will add the cart items to the database -->
           <button type="button" class="btn btn-success btn-lg" ng-click="Checkout()">Checkout</button>
         </div>
       </div>
     </div>
+    <!-- Scripts used -->
     <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/angular.min.js"></script>
--- js/POSController.js
+++ js/POSController.js
@@ -1,41 +1,62 @@
 var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope, $http) {
+
+  //Initialize the cart array on page startup
   $scope.Cart = [];
+
+  //Get items from the json items file to fill the items array (will be replaced with a database)
   $http.get("data/item.json")
   .then(function(response) {
       $scope.Items = response.data;
   });
 
+  //Function called to add items from the input fields to the cart
   $scope.Add = function (Item, Number) {
-      alert("Medicine added to Cart");
+
+    //Initialize error to a null value
     $scope.error = "";
+
+    //Check that the input fields are not empty/valid
     if (Item == null || Number == 0 || Number == null)
       {
+        //If invalid then set error
         $scope.error = "Error: Invalid input, please check that fields contain values";
       }
     else
       {
+        //If valid then retrieve the price of the requested item from the items array
         var price = $scope.GetPrice(Item, Number);
+
+        //Check to see if the cart already contains an entry for the item requested
         var index = $scope.Cart.findIndex(x=>x.name === Item);
+
+        //If the index is -1 then it was not found, if any other value then the cart already contains and entry for the item
         if (index == -1)
           {
+            //Users must request a positive number of items
             if (Number > 0)
               {
+                //Add the item to the cart
                 $scope.Cart.push({name: Item, amount: Number, price: price});
               }
             else
               {
+                //If a negative or 0 is requested spit back an error
                 $scope.error = "Error: Negative purchase being applied to non-existent cart item";
               }
           }
         else
           {
+            //The cart already contains an entry for this item
             if ($scope.Cart[index].amount + Number < 1)
               {
+                //If the new value of the item would make the total requested item number 0 or less
+                //Remove the entry from the cart
                 $scope.Cart.splice(index, 1);
               }
             else
               {
+                //If the request amount would not put the entry to 0 or negative then update it
                 $scope.Cart[index].amount += Number;
                 $scope.Cart[index].price += price;
               }
@@ -43,41 +64,52 @@ app.controller('myCtrl', function($scope, $http) {
       }
   };
 
+  //Called from the html to display the total price of all cart items
   $scope.GetTotal = function () {
+    //Initialize price to 0
     var totalprice = 0;
+
+    //For each item, add it's price to the total
     for (i = 0; i < $scope.Cart.length; i++)
       {
         totalprice += $scope.Cart[i].price;
       }
+
+    //Return the total
     return totalprice;
   };
 
+  //Utility function to return the price of a item in the items array
   $scope.GetPrice = function (Item, Number) {
     var index = $scope.Items.findIndex(x=>x.name === Item);
+
+    //Multiplies the price found by the number of item requested
     var price = $scope.Items[index].price * Number;
     return price;
   };
 
+  //Called from the html to remove an item from the cart array
   $scope.Delete = function (Item) {
     $scope.Cart.splice($scope.Cart.indexOf(Item), 1);
   };
 
+  //Called from the html to clear the cart
   $scope.ClearAll = function () {
     $scope.Cart = [];
     $scope.error = "";
   };
-    
-         $scope.Checkout = function(){  
-          alert("Checkout works");
-         for(i = 0; i < $scope.Cart.length; i++)
-             {
-               $http.post(  
-                    "insert.php",  
-                    {'cartSize':$scope.Cart.length, 'item':$scope.Cart[i].name, 'amount':$scope.Cart[i].amount,
-                    'price':$scope.Cart[i].price }  
-               ).success(function(data){  
-                    alert(data);  
-               });  
-             }
-      } 
+
+  $scope.Checkout = function(){
+    alert("Checkout works");
+    for(i = 0; i < $scope.Cart.length; i++)
+      {
+         $http.post(
+              "insert.php",
+              {'cartSize':$scope.Cart.length, 'item':$scope.Cart[i].name, 'amount':$scope.Cart[i].amount,
+              'price':$scope.Cart[i].price }
+         ).success(function(data){
+              alert(data);
+        });
+      }
+  }
 });
