e7266fb7c50fa4c3a926e7240f4c2c662daa1d9b
Stephen Fleming <100963909@student.swin.edu.au>
2017-09-18 20:34:48 +1000

    Now shows item name instead of id

--- php/getSalesRecords.php
+++ php/getSalesRecords.php
@@ -1,11 +1,16 @@
 <?php
   $db_con = new mysqli("127.0.0.1", "root", "", "PHPSRePS") or die("Unable to connect!");
   $table = "salesRecords";
+
   $result = $db_con->query("SELECT * FROM $table;");
+
   $rows = array();
   while ($r = $result->fetch_assoc()) {
-    $rows[] = $r;
+    @$itemId = $r['item'];
+    @$item = $db_con->query("SELECT * FROM items WHERE (itemId = '$itemId');")->fetch_assoc();
+    @$rows[] = [ "saleNumber" => $r['saleNumber'], "item" => $item['itemName'], "quantity" => $r['quantity'], "date" => $r['date'], "price" => $r['price'] ];
   }
+  
   header('Content-Type: application://json');
   echo json_encode($rows,JSON_NUMERIC_CHECK);
 ?>
